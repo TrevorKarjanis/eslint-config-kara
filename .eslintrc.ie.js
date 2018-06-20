e0c28ba81183717b153b882d24c65a13f96fa1c1
Trevor Karjanis <TrevorKarjanis@users.noreply.github.com>
2018-06-20 15:10:18 -0500

    Fix linter errors in .eslintrc.ie.js.

--- .eslintrc.ie.js
+++ .eslintrc.ie.js
@@ -1,11 +1,11 @@
 {
   // Override eslint-config-kara to enforce JavaScript standards supported by
   // IE 11.
-  "env": {
-    "es6": false
+  env: {
+    es6: false
   },
-  "parserOptions": {
-    "ecmaVersion": 5,
-    "sourceType": "script"
+  parserOptions: {
+    ecmaVersion: 5,
+    sourceType: 'script'
   }
 }
